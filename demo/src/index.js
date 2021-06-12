eb81ec5c8ba5dbaa5385f085a8c694fbff5b7e48
Greg P <gplechaty@gmail.com>
2021-06-11 21:29:13 -0600

    New top-level component created; demo functionality working. CSS correctly applied.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react'
+import React, { useState } from 'react'
 import {render} from 'react-dom'
+//TODO: SOME KIND OF LOGO 
+//import logo from './logo.svg';
+//TODO if necessary::::: import './index.css';
 
-import Example from '../../src'
+import {CurrencyCompareBarChart} from '../../src';
 
+function App() {
+  
+    return (
+    <div>
+      <h1>currency-compare-bar-chart Demo</h1>
+      <CurrencyCompareBarChart/>
+    </div>
+      )
+  };
+  
+
+/*
 export default class Demo extends Component {
   render() {
     return <div>
       <h1>currency-compare-bar-chart Demo</h1>
-      <Example/>
+      <CurrencyCompareBarChart/>
     </div>
-  }
-}
+  }*/
 
-render(<Demo/>, document.querySelector('#demo'))
+
+render(<CurrencyCompareBarChart/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React, {Component} from 'react'
+import CurrencyCompareBarChart from './CurrencyCompareBarChart/CurrencyCompareBarChart/CurrencyCompareBarChart.js';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+export {  CurrencyCompareBarChart  }
